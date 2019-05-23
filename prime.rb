35f2e91c501d0129eb2d3c6316855cd13c35e6ad
Eliot Howes <ejhowes@gmail.com>
2019-05-23 22:50:46 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(number)
+  if number < 2
+    return false
+  end
+
+  (2..(number - 1)).each do |n|
+    if number % n == 0
+        return false
+    end
+  end
+  true
+end
